ce19e3a34baeb7feb098e46b1e09a9e18ecf8fcb
riya <riyaaaaa1610@gmail.com>
2026-09-15 21:24:59 +0530

    made changes to cargo type in prediction.py

--- prediction.py
+++ prediction.py
@@ -1,22 +1,55 @@
 from viability import predict_viability
 
+
+CARGO_PARAMETERS = {
+    "Solid Organs": {
+        "safe_temp": 5,
+        "max_temp_threshold": 8,
+    },
+
+    "Platelets": {
+        "safe_temp": 22,
+        "max_temp_threshold": 24,
+    },
+
+    "Red Blood Cells": {
+        "safe_temp": 5,
+        "max_temp_threshold": 10,
+    },
+
+    "Plasma & Cryo": {
+        "safe_temp": -18,
+        "max_temp_threshold": -18,
+    },
+
+    "Skin, Bones, Valves": {
+        "safe_temp": -40,
+        "max_temp_threshold": -40,
+    }
+}
+
+
 DEFAULT_DECAY_RATE = 0.02
 
 
 def get_prediction(
+    cargo_type,
     current_temp,
-    safe_temp,
-    max_temp_threshold,
     flight_delay_minutes
 ):
     """
-    Get viability prediction using the default decay rate.
+    Get viability prediction based on cargo type.
     """
+
+    if cargo_type not in CARGO_PARAMETERS:
+        raise ValueError("Unknown cargo type")
+
+    parameters = CARGO_PARAMETERS[cargo_type]
 
     result = predict_viability(
         current_temp=current_temp,
-        safe_temp=safe_temp,
-        max_temp_threshold=max_temp_threshold,
+        safe_temp=parameters["safe_temp"],
+        max_temp_threshold=parameters["max_temp_threshold"],
         flight_delay_minutes=flight_delay_minutes,
         decay_rate=DEFAULT_DECAY_RATE
     )
@@ -27,10 +60,9 @@ def get_prediction(
 if __name__ == "__main__":
 
     result = get_prediction(
-    current_temp=5,
-    safe_temp=5,
-    max_temp_threshold=8,
-    flight_delay_minutes=1200
-)
+        cargo_type="Solid Organs",
+        current_temp=7,
+        flight_delay_minutes=600
+    )
 
     print(result)
